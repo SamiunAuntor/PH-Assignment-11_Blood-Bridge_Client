42e292ce33c2f5a8dab4ef0ca60d4ebf637f6066
Samiunauntor <samiunalimauntor@gmail.com>
2026-01-10 20:07:43 +0600

    made client side deployment ready

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // create instance
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || `http://localhost:5000/`,
+    baseURL: import.meta.env.VITE_API_URL || `https://blood-bridge-server-five.vercel.app`,
 });
 
 const useAxios = () => {
